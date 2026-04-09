bd859ba3e05b365fe13b8d0d27f0121747f6c94f
jgillispie <jack.gillispie15@gmail.com>
2026-04-09 18:01:20 -0400

    backup progress

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-\## I'm playing around with writing custom eBird alerts to stay updated on observations near me
+### This tool will ultimately be a way to automate customer email alerts for eBird sightings.
+#### The idea is to use API specifications and Pandas to aggregate and analyze data, then format the result in an accessible way.
+#### Will eventually have some CLI parameters that take the following  into account: 
 
+- Location + radius
+- Specific species / groups
+- Time Span
+- 
 
-
-\###I'm also doing this to pull enough data to look for patterns in locations / species
-
+#### Getting more familiar with writing tests and GitHub Actions
--- b.py
+++ b.py
@@ -1,4 +1,6 @@
 """
+For some reason w+ yields no permission errors, while w does
+
 TODO: Write multiple data handlers w/ command line options
 TODO: Send visuals along with data pairs
 TODO: Experiment with alternative email servers (other than gmail)
@@ -13,6 +15,7 @@ EMAILPASS:  a password to the gmail you are using to send data
 TOKEN:      a valid eBird token for use with the API
 
 To do this in Windows reliably, use the following commands in terminal
+
 cd ./path/to/your/project
 touch .env
 notepad .env  (or whatever editor you use)
@@ -49,7 +52,7 @@ parser.add_argument(
     '-r',
     nargs='?',
     default= os.getenv('DEFAULT'),
-    help='the email you want the alert sent to\n defaults to the email set in your .env file')#string type assumed as default
+    help='the recipient email you want the alert sent to\n defaults to the email set in your .env file')#string type assumed as default
 args = parser.parse_args()
 
 
@@ -58,18 +61,17 @@ args = parser.parse_args()
 def getNearby():
 
         #Getting the data
-        url = 'https://api.ebird.org/v2/data/obs/geo/recent?lat=35.2916363&lng=-80.7269854&includeProvisional=true&back=1&maxResults=20'
+        url = 'https://api.ebird.org/v2/data/obs/geo/recent?lat=35.2916363&lng=-80.7269854&includeProvisional=true&back=1&maxResults=50'
         headers = {'x-ebirdapitoken': os.getenv('TOKEN')}
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
             raise ValueError(f'The response status was {response.status_code}')
-            return
+            return "Handled Error"
         data = response.json()
-        
         #saving data to file
-        with open('alertData', 'w') as file:
+        with open('C:/Users/jackg/Desktop/b/alertData.txt', 'w+') as file:
                 json.dump(data, file)
-            
+
         #transforming the data from an array of dictionaries to string of meaningful info
         index = [elem['obsDt'] for elem in data] # list of strings
         columns = [species['comName']for species in data]
@@ -77,12 +79,12 @@ def getNearby():
         counts = test.value_counts() # shows counts per species
         v = pd.DataFrame(data=counts, index=test.array) #test.array shows the keys as a list
         body = v.to_string() #to_html seems to send the text of html version
-        
+        body = v.to_html()
         return body
 
-#sends data by email to designated recipient from env set gmail  
+#sends data by email to designated recipient from env set gmail
 def sendData(RECIPIENT, body):
-        
+
     SMTP_SERVER = 'smtp.gmail.com'
     SMTP_PORT = 587
     EMAIL_ADDR = os.getenv('EMAIL')
@@ -92,16 +94,18 @@ def sendData(RECIPIENT, body):
     message.add_header('From', EMAIL_ADDR)
     message.add_header('To', RECIPIENT)
     message.add_header('Subject','Bird Sightings')
-    message.set_content(body)
-    print(message['Content-Type'])
+    message.add_header('Content-Type', 'text/html; charset="utf-8"')
+    message.set_content(body, subtype='html')
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as mailserver:
             mailserver.starttls()
             mailserver.login(EMAIL_ADDR, EMAIL_PASS)
+            #mailserver.sendmail(EMAIL_ADDR, RECIPIENT, message)
             mailserver.sendmail(EMAIL_ADDR, RECIPIENT, message.as_string())
             print('finished')
-    except SMTPException as e:
+            return 1
+    except smtplib.SMTPException as e:
         print(e)
         return
 
@@ -110,9 +114,3 @@ def sendData(RECIPIENT, body):
 if __name__=='__main__':
     data = getNearby()
     sendData(args.r, data)
-
-
-
-
-
-
